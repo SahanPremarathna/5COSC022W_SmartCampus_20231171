1de67b187b337ac19ce41af5d415c84a381eee89
SahanPremarathna <sahanpremarathna2005@gmail.com>
2026-03-06 23:16:03 +0530

    test message

---
+++ src/main/java/com/mycompany/cosc022w_smartcampus_20231171/test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cosc022w_smartcampus_20231171;
+
+/**
+ *
+ * @author sahan
+ */
+public class test {
+    
+}
